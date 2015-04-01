8b7bfc3f10944ca656ae02d03e288174269f6ca1
lancasja <lancasja@gmail.com>
2015-04-01 15:58:14 -0700

    Initial commit

---
+++ www/js/app.js
@@ -0,0 +1,86 @@
+/*
+  420 Radio App
+  Made with Ionic Framework
+  v0.0.0
+*/
+
+/* ================== */
+/* == GLOBAL SPACE == */
+/* ================== */
+// For creating, registering and retrieving Angular modules
+// The 2nd parameter is an array of 'requires'
+// 'radioLive.controllers' is found in controllers.js
+// Installed ngCordova [http://ngcordova.com/]
+angular.module('radioLive', ['ionic', 'radioLive.controllers', 'ngCordova'])
+
+/* ============= */
+/* == STARTUP == */
+/* ============= */
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+/* ============ */
+/* == ROUTES == */
+/* ============ */
+.config(function($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+
+  .state('app', {
+    url: "/app",
+    abstract: true,
+    templateUrl: "templates/menu.html",
+    controller: 'AppCtrl'
+  })
+
+  .state('app.stream', {
+    url: "/stream",
+    views: {
+      'menuContent': {
+        templateUrl: "templates/stream.html",
+        controller: 'StreamCtrl'
+      }
+    }
+  })
+
+  .state('app.legal', {
+    url: "/legal",
+    views: {
+      'menuContent': {
+        templateUrl: "templates/legal.html"
+      }
+    }
+  })
+    .state('app.episodes', {
+      url: "/episodes",
+      views: {
+        'menuContent': {
+          templateUrl: "templates/episodes.html",
+          controller: 'EpisodesCtrl'
+        }
+      }
+    })
+
+  .state('app.single', {
+    url: "/episodes/:episodeId",
+    views: {
+      'menuContent': {
+        templateUrl: "templates/episode.html",
+        controller: 'EpisodeCtrl'
+      }
+    }
+  });
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/app/stream');
+});
---
+++ www/js/controllers.js
@@ -0,0 +1,147 @@
+angular.module('radioLive.controllers', [])
+
+/* ===================== */
+/* == MAIN CONTROLLER == */
+/* ===================== */
+.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+
+  /*
+    Login functionality here by default.
+  */
+
+  // Form data for the login modal
+  $scope.loginData = {};
+
+  // Triggered in the login modal to close it
+  $scope.closeLogin = function() {
+    $scope.modal.hide();
+  };
+
+  // Open the login modal
+  $scope.login = function() {
+    $scope.modal.show();
+  };
+
+  // Perform the login action when the user submits the login form
+  $scope.doLogin = function() {
+    console.log('Doing login', $scope.loginData);
+
+    // Simulate a login delay. Remove this and replace with your login
+    // code if using a login system
+    $timeout(function() {
+      $scope.closeLogin();
+    }, 1000);
+  };
+})
+
+/* ============================= */
+/* == AUDIO STREAM CONTROLLER == */
+/* ============================= */
+.controller('StreamCtrl', function($scope, $http, $cordovaMedia) {
+
+  /*
+    HTML5 Audio
+    [https://developer.mozilla.org/en-US/docs/Web/API/HTMLAudioElement]
+
+    Cordova Background Audio
+    [http://plugins.cordova.io/#/package/com.cordova.background-audio]
+  */
+
+  /*
+    Url taken from PLS file downloaded from SHOUTcast
+    [http://yp.shoutcast.com/sbin/tunein-station.pls?id=172366]
+
+    - TODO: Request the PLS file and parse it's contents to get the Url. Right now I'm just downloading the file, opening it in a text editor and getting the Url. But it seems the server IP changes every now and then.
+  */
+  var url = 'http://209.95.38.207:8000/';
+  var audioUrl = url + ';type=mp3';
+  var metaUrl = url + 'stats?sid=1';
+
+  var stream = new Audio(audioUrl);
+
+  /*
+    Get metadata from SHOUTcast server
+    [http://learn.ionicframework.com/formulas/backend-data/]
+  */
+  var getMeta = function() {
+    $http.get(metaUrl)
+      .success(function(data) {
+
+        // TODO: Implement x2js [https://code.google.com/p/x2js/]
+        var title = data.substring(data.indexOf('<SONGTITLE>') + 11, data.indexOf('</SONGTITLE>'));
+
+        // Add to scope to use in view
+        $scope.metadata = title;
+      })
+      .error(function(errData) {
+        alert("Sorry, we can't get the currently playing info for you at the moment.");
+        console.log('ERROR getting data from [ ' + metaUrl + ' ]:\n' + errData);
+      });
+  }
+
+  // Set metadata right off the bat
+  getMeta();
+
+  // Update metadata every 5 seconds
+  setInterval(getMeta, 5000);
+
+  /* TODO:
+      - Make these a Play/Pause button
+      - Add a rewind button
+  */
+  // Play button
+  $scope.playStream = function() {  
+    if (stream.paused) {
+      stream.play();
+    }
+  };
+
+  // Pause button
+  $scope.pauseStream = function() {
+    if (!stream.paused) {
+      stream.pause();
+    }
+  };
+})
+
+/* ============================= */
+/* == EPISODE LIST CONTROLLER == */
+/* ============================= */
+.controller('EpisodesCtrl', function($scope) {
+  $scope.episodes = [
+    { title: 'Reggae', id: 1 },
+    { title: 'Chill', id: 2 },
+    { title: 'Dubstep', id: 3 },
+    { title: 'Indie', id: 4 },
+    { title: 'Rap', id: 5 },
+    { title: 'Cowbell', id: 6 }
+  ];
+})
+
+/* =============================== */
+/* == SINGLE EPISODE CONTROLLER == */
+/* =============================== */
+.controller('EpisodeCtrl', function($scope, $stateParams) {
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
